2a505c9c61addc73a2707afc38ea6fdad604c291
simon <simonnfts@gmail.com>
2023-10-23 19:30:18 -0400

    removing console and editing settings

--- src/libraries/VestingMathLib.sol
+++ src/libraries/VestingMathLib.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/console.sol";
 
 uint256 constant VESTING_PERIODS = 100;
 /// @dev the maximum amount of seconds a second can vest for
