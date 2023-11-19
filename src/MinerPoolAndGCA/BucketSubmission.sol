d7d66a56896fcee0f39b13d7ba7fb4a716633d1c
simon <simonnfts@gmail.com>
2023-11-19 02:20:08 -0500

    removing unused import

--- src/MinerPoolAndGCA/BucketSubmission.sol
+++ src/MinerPoolAndGCA/BucketSubmission.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {IMinerPool} from "@/interfaces/IMinerPool.sol";
-
 contract BucketSubmission {
     /**
      * @notice the start offset to the current bucket for the grc deposit
