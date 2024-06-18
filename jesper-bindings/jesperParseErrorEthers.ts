8122259fc609d484fc488afb0faf6cff5fd7ef61
simon <simonnfts@gmail.com>
2024-06-18 14:50:35 -0400

    creating jesper bindings and debug script

--- jesper-bindings/jesperParseErrorEthers.ts
+++ jesper-bindings/jesperParseErrorEthers.ts
@@ -1,39 +1,41 @@
-import { ethers } from 'ethers';
-import { errors } from './jesper-bindings';
 
-type DebugArg = {
-  value: string;
-  name: string;
-};
-export const jesperParseError = (errorData: string) => {
-  const first4Bytes = errorData.slice(0, 10);
-  const error = errors[first4Bytes];
-  if (!error) {
-    throw new Error(`Unknown error: ${errorData}`);
-  }
-
-  const abiCoder = new ethers.utils.AbiCoder();
-
-  let errorMessage = error.solidityMessageAndArgs.errorMessage;
-
-  const _errorData = `0x${errorData.slice(10)}`;
-
-  let debugArgs: DebugArg[] = [];
-  if (error.solidityMessageAndArgs.args.length > 0) {
-    const decoded = abiCoder.decode(
-      error.solidityMessageAndArgs.args.map((arg) => arg.type),
-      _errorData,
-    );
-    error.solidityMessageAndArgs.args.forEach((arg, i) => {
-      const value =
-        typeof decoded[i] === 'string' ? decoded[i] : decoded[i].toString();
-      errorMessage = errorMessage.replace(`{${arg.name}}`, value);
-      debugArgs.push({ name: arg.name, value });
-    });
-  }
-  return {
-    error,
-    errorMessage,
-    debugArgs,
-  };
-};
+    import { ethers } from "ethers";
+    import { errors } from "./jesper-bindings";
+    
+    type DebugArg = {
+      value: string;
+      name: string;
+    };
+    export const jesperParseError = (errorData: string) => {
+      const first4Bytes = errorData.slice(0, 10);
+      const error = errors[first4Bytes];
+      if (!error) {
+        throw new Error(`Unknown error: ${errorData}`);
+      }
+    
+      const abiCoder = new ethers.utils.AbiCoder();
+    
+      let errorMessage = error.solidityMessageAndArgs.errorMessage;
+    
+      const _errorData = `0x${errorData.slice(10)}`;
+    
+      let debugArgs: DebugArg[] = [];
+      if (error.solidityMessageAndArgs.args.length > 0) {
+        const decoded = abiCoder.decode(
+          error.solidityMessageAndArgs.args.map((arg) => arg.type),
+          _errorData
+        );
+        error.solidityMessageAndArgs.args.forEach((arg, i) => {
+          const value =
+            typeof decoded[i] === "string" ? decoded[i] : decoded[i].toString();
+          errorMessage = errorMessage.replace(`{${arg.name}}`, value);
+          debugArgs.push({ name: arg.name, value });
+        });
+      }
+      return {
+        error,
+        errorMessage,
+        debugArgs,
+      };
+    };
+    
--- test/Integrations/Debug/DebugClaim.t.sol
+++ test/Integrations/Debug/DebugClaim.t.sol
@@ -1,46 +1,133 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-// import "forge-std/Script.sol";
-// import "forge-std/console.sol";
-// // import {GCC} from "@/GCC.sol";
-// // import {TestGLOW} from "@/testing/TestGLOW.sol";
-// // import {GoerliGovernanceQuickPeriod} from "@/testing/Goerli/GoerliGovernance.QuickPeriod.sol";
-// // import {GoerliGCC} from "@/testing/Goerli/GoerliGCC.sol";
-// // import {MockUSDC} from "@/testing/MockUSDC.sol";
-// // import {EarlyLiquidity} from "@/EarlyLiquidity.sol";
-// // import {IUniswapRouterV2} from "@/interfaces/IUniswapRouterV2.sol";
-// // import {CarbonCreditDutchAuction} from "@/CarbonCreditDutchAuction.sol";
-// // import {GoerliMinerPoolAndGCAQuickPeriod} from "@/testing/Goerli/GoerliMinerPoolAndGCA.QuickPeriod.sol";
-// // import {VetoCouncil} from "@/VetoCouncil.sol";
-// // import {HoldingContract} from "@/HoldingContract.sol";
-// // import {GrantsTreasury} from "@/GrantsTreasury.sol";
-// // import {BatchCommit} from "@/BatchCommit.sol";
-// import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+// import {GCC} from "@/GCC.sol";
+// import {TestGLOW} from "@/testing/TestGLOW.sol";
+// import {GoerliGovernanceQuickPeriod} from "@/testing/Goerli/GoerliGovernance.QuickPeriod.sol";
+// import {GoerliGCC} from "@/testing/Goerli/GoerliGCC.sol";
+// import {MockUSDC} from "@/testing/MockUSDC.sol";
+// import {EarlyLiquidity} from "@/EarlyLiquidity.sol";
+// import {IUniswapRouterV2} from "@/interfaces/IUniswapRouterV2.sol";
+// import {CarbonCreditDutchAuction} from "@/CarbonCreditDutchAuction.sol";
+// import {GoerliMinerPoolAndGCAQuickPeriod} from "@/testing/Goerli/GoerliMinerPoolAndGCA.QuickPeriod.sol";
+// import {VetoCouncil} from "@/VetoCouncil.sol";
+// import {HoldingContract} from "@/HoldingContract.sol";
+// import {GrantsTreasury} from "@/GrantsTreasury.sol";
+// import {BatchCommit} from "@/BatchCommit.sol";
+import {MinerPoolAndGCA} from "@/MinerPoolAndGCA/MinerPoolAndGCA.sol";
+import "forge-std/Test.sol";
 
-// contract Debug2 is Test {
-//     string goerliForkUrl = vm.envString("GOERLI_RPC_URL");
-//     uint256 goerliFork;
-//     address me = 0xD509A9480559337e924C764071009D60aaCA623d;
-//     address minerPoolGoerli = 0xa2126e06AF1C75686BCBAbb4cD426bE35aEECC0C;
+contract Debug2 is Test {
+    string mainnetForkUrl = vm.envString("MAINNET_RPC");
+    uint256 mainnetFork;
+    address gca = 0xB2d687b199ee40e6113CD490455cC81eC325C496;
+    address farm = 0x7781182412aC86aC1a1a50686847728002211882;
+    MinerPoolAndGCA minerPoolAndGCA = MinerPoolAndGCA(0x6Fa8C7a89b22bf3212392b778905B12f3dBAF5C4);
+  /*
+  {
+  "weekNumber": 19,
+  "totalCreditsProduced": 0.4970041288201747,
+  "totalCreditsProducedBN": "497004128820174700",
+  "totalGlowWeightInFinalized": "350581250",
+  "totalGlowWeightHuman": "350.58125",
+  "totalGCCWeightInFinalized": "497004",
+  "totalGCCWeightHuman": "0.497004",
+  "root": "0xc13bb1d37a18d61b0ce3a77ce447936444adf3768e1fdbb0300c34a7db565c43"
+}
+*/
 
-//     function setUp() public {
-//         goerliFork = vm.createFork(goerliForkUrl);
-//         vm.selectFork(goerliFork);
-//     }
+/*
+   {
+        "wallet": "0xCB0695C5e231D04a36feb07841e26D44e6D08c9d",
+        "glowWeight": "65935096",
+        "usdgWeight": "65920",
+        "proof": [
+            "0x3b3366fa05f2f8045b29f89fa869b7f68385d40acfe190a83951189df12ab6ff",
+            "0xa2cab092ec2b91edd76cadb016c3d7b301d4e3e1ed718da9eee197931529150b",
+            "0xd10d4cae3a3a3d6a3c74cbd373536eec3998304dc199c02848c8ab114389e278"
+        ]
+    }
+    */
+    function setUp() public {
+        mainnetFork = vm.createFork(mainnetForkUrl);
+        vm.selectFork(mainnetFork);
+    }
+    
+    function testForkChangeGCAs() public {
+        address[] memory gcasToSlash = new address[](0);
+        address[] memory newGCAs = new address[](2);
+        newGCAs[0] = 0xB2d687b199ee40e6113CD490455cC81eC325C496;
+        newGCAs[1] = 0x63a74612274FbC6ca3f7096586aF01Fd986d69cE;
+        uint256 proposalCreationTimestamp = 1713556415;
+        minerPoolAndGCA.executeAgainstHash(gcasToSlash, newGCAs, proposalCreationTimestamp);
+    }
 
-//     function test_goerliClaimBucket_debug() public {
-//         vm.startPrank(me);
-//         bytes memory data =
-//             hex"d004f0f7000000000000000000000000d509a9480559337e924c764071009d60aaca623d00000000000000000000000000000000000000000000000000000000000f4240";
-//         address to = 0x7734720e7Cea67b29f53800C4aD5C40e61aBb645;
+    function test_claimWeek23() public {
 
-//              (bool success, bytes memory returnData) = address(to).call(data);
-//         if(!success) {
-//            assembly {
-//                 revert(add(returnData, 0x20), mload(returnData))
-//            }
-//         }
-//         vm.stopPrank();
-//     }
-// }
+        vm.startPrank(farm);
+        /*
+
+bucketNumber
+: 
+23
+glowWeight
+: 
+"68627700"
+indexOfGCA
+: 
+0
+payoutWallet
+: 
+"0x7781182412aC86aC1a1a50686847728002211882"
+proof
+: 
+Array(4)
+0
+: 
+"0xed30f9fbb32b7fed702ac387c51155312c5e61e79405dbf1f024585feb0b03d9"
+1
+: 
+"0x4ee0c843256916e4e8787dad8a54cbdc7bc7fcdb602c3741dd07f3ed1ada17ea"
+2
+: 
+"0xeffd64e6c57ead8a5c80b6ffc975be73d313fb6fd70a0a582601d07f3bbb3661"
+3
+: 
+"0x87aa313f618efd96556168344d5721069b30545b4f769f4c49ef1752203eb604"
+length
+: 
+4
+[[Prototype]]
+: 
+Array(0)
+usdcWeight
+: 
+"70555"
+*/
+        bytes32[] memory proof = new bytes32[](4);
+        proof[0] = 0xed30f9fbb32b7fed702ac387c51155312c5e61e79405dbf1f024585feb0b03d9;
+        proof[1] = 0x4ee0c843256916e4e8787dad8a54cbdc7bc7fcdb602c3741dd07f3ed1ada17ea;
+        proof[2] = 0xeffd64e6c57ead8a5c80b6ffc975be73d313fb6fd70a0a582601d07f3bbb3661;
+        proof[3] = 0x87aa313f618efd96556168344d5721069b30545b4f769f4c49ef1752203eb604;
+
+        uint256 farmGlowWeight = 68627700;
+        uint256 farmUsdgWeight = 70555;
+
+        minerPoolAndGCA.claimRewardFromBucket({
+            bucketId: 23,
+            glwWeight: farmGlowWeight,
+            usdcWeight: farmUsdgWeight,
+            proof: proof,
+            index: 0,
+            user: farm,
+            claimFromInflation: false,
+            signature: ""
+        });
+
+        vm.stopPrank();
+
+
+    }
+}
