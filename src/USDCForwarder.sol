231c789b8eb476984b93de00ebe8d06b3b009512
0xSimon <simonnfts@gmail.com>
2025-07-18 10:35:22 -0400

    nit

--- src/USDCForwarder.sol
+++ src/USDCForwarder.sol
@@ -8,15 +8,15 @@ contract USDCForwarder {
     address public immutable usdc;
     address public immutable FORWARD_ADDRESS;
 
-    event Forward(address indexed from, address indexed to, int256 amount, string message);
+    event Forward(address indexed from, address indexed to, uint256 amount, string message);
 
     constructor(address _usdc, address _usdcForwarder) {
         usdc = _usdc;
         FORWARD_ADDRESS = _usdcForwarder;
     }
 
-    function forward(int256 amount, string memory message) external {
-        SafeERC20.safeTransferFrom(IERC20(usdc), msg.sender, FORWARD_ADDRESS, uint256(amount));
+    function forward(uint256 amount, string memory message) external {
+        SafeERC20.safeTransferFrom(IERC20(usdc), msg.sender, FORWARD_ADDRESS, amount);
         emit Forward(msg.sender, FORWARD_ADDRESS, amount, message);
     }
 }
